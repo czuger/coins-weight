59aaa98524b0bc090f38768d25697035eff4acae
ced <zuger.cedric@gmail.com>
2017-11-20 14:25:39 +0100

    Total included.

--- scripts/coins_weight.lua
+++ scripts/coins_weight.lua
@@ -3,15 +3,26 @@
 -- attribution and copyright information.
 --
 
--- Skill properties
-
 -- Initialization
 function onInit()
 	if User.isHost() then
 		Comm.registerSlashHandler("ccweight", computeCoinsWeight);
+		-- computeCoinsWeight();		
 	end
 end
+
+-- This function recompute the total weight field						
+function recomputeTotalWeight( nodeWin )
+	local rActor = ActorManager.getActor("pc", nodeWin ); 
+	local nodePC = DB.findNode(rActor['sCreatureNode']);
+	
+	local treasure = DB.getValue( nodePC.getPath() .. '.encumbrance.treasure' );						
+	local eqload = DB.getValue( nodePC.getPath() .. '.encumbrance.load' );
+	
+	DB.setValue( nodePC.getPath() .. '.encumbrance.total', 'number', treasure+eqload );
+end						
 			
+-- This function is manualy called with the command /ccweight (DM only)		
 function computeCoinsWeight(command, parameters)
 	if User.isHost() then
 		for _,v in pairs(DB.getChildren("partysheet.partyinformation")) do
@@ -27,12 +38,14 @@ function computeCoinsWeight(command, parameters)
 	end
 end
 
+-- This function is called when a coin field is called
 function onCoinsValueChanged( nodeWin )
 	local rActor = ActorManager.getActor("pc", nodeWin ); 
 	local nodePC = DB.findNode(rActor['sCreatureNode']);
 	CoinsWeight.computePCCoinsWeigh( nodePC );
 end
 
+-- This function really compute the weight of the coins
 function computePCCoinsWeigh( nodePC )
 	local weight = 0;
 	for _,coin in pairs(DB.getChildren(nodePC, "coins")) do
